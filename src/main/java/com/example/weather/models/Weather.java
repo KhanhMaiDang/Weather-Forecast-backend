1543adac6cbae0cea455ac61619e76f83e5768e0
Nhat Tan <66843927+lonhattan007@users.noreply.github.com>
2023-05-14 14:37:43 +0700

    Changed some data fields and collection mapping

--- src/main/java/com/example/weather/models/Weather.java
+++ src/main/java/com/example/weather/models/Weather.java
@@ -8,50 +8,83 @@ import org.springframework.data.mongodb.core.mapping.Field;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
-@Getter @Setter
-@Document(collection = "weather_1H")
+@Getter
+@Setter
+@Document(collection = "weather")
 //@Document(collection = "weather_test") // mongoDB local
-public class  Weather {
+public class Weather {
     private String _id;
 
     @Field(name = "date_time")
     private LocalDateTime dateTime;
 
-    private double precipMM;
+//    @Field(name = "location_id")
+//    private String locationId;
+
+    private String location;
+
+    @Field(name = "truth_location")
+    private String truthLocation;
+
+    @Field(name = "weather_status")
+    private String weatherStatus;
+
+    private int tempC;
+
+    @Field("FeelsLikeC")
+    private int feelsLikeC;
+
+    @Field(name = "HeatIndexC")
+    private int heatIndexC;
+
+    @Field(name = "maxtempC")
+    private int maxTempC;
+
+    @Field(name = "mintempC")
+    private int minTempC;
+
+    @Field(name = "DewPointC")
+    private int dewPointC;
+
+    @Field(name = "WindChillC")
+    private int windChillC;
+
+    @Field(name = "windspeedKmph")
+    private int windSpeedKmph;
 
     @Field(name = "WindGustKmph")
     private int windGustKmph;
-    @Field(name = "DewPointC")
-    private int dewPointC;
+
+    @Field(name = "winddirDegree")
+    private int windDirDegree;
+
+    private int humidity;
+
     private int pressure;
-    private int maxtempC;
-    private int cloudcover;
+
     @Field(name = "totalSnow_cm")
     private double totalSnowCm;
-    @Field(name = "windspeedKmph")
-    private int windSpeedKmph;
-    private String location;
 
-    @Field(name="location_id")
-    private String locationId;
+    @Field(name = "precipMM")
+    private double precipitationMm;
+
+    private int visibility;
+
+    @Field(name = "cloudcover")
+    private int cloudCover;
+
+    private int uvIndex;
+
+    private double sunHour;
+
+    private String sunrise;
+
+    private String sunset;
+
+    private String moonrise;
+
+    private String moonset;
+
     @Field(name = "moon_illumination")
     private int moonIllumination;
-    private int tempC;
-    private int humidity;
-    private int uvIndex;
-    @Field("FeelsLikeC")
-    private int feelsLikeC;
-    private int mintempC;
-    private String moonset;
-    private String sunset;
-    private int visibility;
-    @Field(name = "HeatIndexC   ")
-    private int heatIndexC;
-    private String sunrise;
-    private int winddirDegree;
-    private double sunHour;
-    @Field(name = "weather_status")
-    private String weatherStatus;
-    @Field(name = "avgtempC")
-    private double avgTempC;
 }

d24bfa903bc8ef5d6cee32fac2df76ce7ee50146
KhanhMai <dkmai18@apcs.vn>
2023-05-17 11:31:28 +0700

    GET-method-get-hourly-weather-prediction

--- src/main/java/com/example/weather/services/WeatherForecastingService.java
+++ src/main/java/com/example/weather/services/WeatherForecastingService.java
@@ -1,4 +1,9 @@
 package com.example.weather.services;
 
+import com.example.weather.models.WeatherForecasting;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface WeatherForecastingService {
+    List<WeatherForecasting> getHourlyWeatherForecast(String locationName, LocalDateTime startDateTime);
 }
--- src/main/java/com/example/weather/services/implementation/WeatherForecastingServiceImpl.java
+++ src/main/java/com/example/weather/services/implementation/WeatherForecastingServiceImpl.java
@@ -1,7 +1,30 @@
 package com.example.weather.services.implementation;
 
+import com.example.weather.models.WeatherForecasting;
+import com.example.weather.repository.WeatherForecastingRepository;
 import com.example.weather.services.WeatherForecastingService;
+import com.example.weather.utils.StringUtils;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class WeatherForecastingServiceImpl implements WeatherForecastingService {
 
+    private final WeatherForecastingRepository weatherForecastingRepository;
+
+    public WeatherForecastingServiceImpl(WeatherForecastingRepository weatherForecastingRepository) {
+        this.weatherForecastingRepository = weatherForecastingRepository;
+    }
+
+    @Override
+    public List<WeatherForecasting> getHourlyWeatherForecast(String locationName, LocalDateTime startDateTime) {
+        LocalDateTime endDaTeTime = startDateTime.plusDays(1);
+        locationName = StringUtils.removeAccent(locationName);
+
+        List<WeatherForecasting> forecastingList = weatherForecastingRepository.findWeatherForecastingByLocationAndDateTimeBetween(locationName, startDateTime, endDaTeTime);
+
+        return forecastingList;
+    }
 }

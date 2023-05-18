0079f37cf55a0db7ab8f9338715db7698fff7ed9
HUYNHTUAN\PC <huynhanhtuan02.tv@gmail.com>
2023-05-18 18:41:47 +0700

    Update WeatherForecastingController

--- src/main/java/com/example/weather/controller/WeatherForecastingController.java
+++ src/main/java/com/example/weather/controller/WeatherForecastingController.java
@@ -4,16 +4,14 @@ import com.example.weather.models.DayForecasting;
 import com.example.weather.models.WeatherForecasting;
 import com.example.weather.services.WeatherForecastingService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/weather/forecast")
+@CrossOrigin(origins = "*")
 public class WeatherForecastingController {
     private final WeatherForecastingService forecastingService;
 
